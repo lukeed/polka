e3c6966bf21c0ad7b2cb9ff96120208b22d9333c
Luke Edwards <luke.edwards05@gmail.com>
2018-01-22 11:51:52 -0800

    refactor: allow sub-apps to route to own handlers;
    - breaking: sub-apps require a basepath to be mounted now
    - searches for sub-apps or uri-assigned middleware groups
    - mutates the `req.url` once a sub-level has been entered
    - passes the parsed `info` to sub-app to save an fn call
    - adds a `req.originalUrl` value after mutating `req.url`
    - add `onNoMatch` option handler w/ curried default
    - simplify `use()` & handle uri base (closes #8)

--- packages/polka/index.js
+++ packages/polka/index.js
@@ -2,6 +2,15 @@ const http = require('http');
 const Router = require('trouter');
 const parseurl = require('parseurl');
 
+function strip(x) {
+	return x.charCodeAt(0) === 47 ? x.substring(1) : x;
+}
+
+function value(x) {
+  let y = x.indexOf('/', 1);
+  return y > 1 ? x.substring(1, y) : x.substring(1);
+}
+
 function onError(err, req, res, next) {
 	let code = (res.statusCode = err.code || err.status || 500);
 	res.end(err.message || err.toString() || http.STATUS_CODES[code]);
@@ -10,27 +19,30 @@ function onError(err, req, res, next) {
 class Polka extends Router {
 	constructor(opts={}) {
 		super(opts);
+		this.apps = {};
 		this.wares = [];
+		this.bwares = {};
 		this.parse = parseurl;
 		this.listen = this.start;
 		this.handler = this.handler.bind(this);
 		this.server = http.createServer(this.handler);
 		this.onError = opts.onError || onError; // catch-all handler
+		this.onNoMatch = opts.onNoMatch || this.onError.bind(null, { code:404 });
 	}
 
-	use(...fns) {
-		fns.forEach(fn => {
-			if (fn instanceof Polka) {
-				let m, keys, obj=fn.handlers;
-				for (m in obj) {
-					if ((keys=Object.keys(obj[m])).length) {
-						keys.forEach(uri => this.add(m, uri, obj[m][uri]));
-					}
+	use(base, ...fns) {
+		if (typeof base === 'function') {
+			this.wares = this.wares.concat(base, fns);
+		} else {
+			base = strip(base);
+			fns.forEach(fn => {
+				if (fn instanceof Polka) {
+					this.apps[base] = fn;
+				} else {
+					this.bwares[base] = (this.bwares[base] || []).concat(fn);
 				}
-			} else {
-				this.wares.push(fn);
-			}
-		});
+			});
+		}
 		return this; // chainable
 	}
 
@@ -51,17 +63,27 @@ class Polka extends Router {
 
 	handler(req, res, info) {
 		info = info || this.parse(req);
-		// Only do work if route is found~!
-		let obj = this.find(req.method, info.pathname);
-		if (!obj) return this.send(res, 501);
-		// Provide `req.params`
-		req.params = obj.params;
+		let arr=this.wares,
+			obj=this.find(req.method, info.pathname);
+		if (obj) {
+			req.params = obj.params;
+		} else {
+			// Looking for sub-apps or extra middleware
+			let base = value(info.pathname);
+			req.originalUrl = req.url; // for somebody?
+			info.pathname = req.url = '/' + strip(req.url.substring(base.length + 1));
+			if (this.apps[base] !== void 0) {
+				return this.apps[base].handler(req, res, info);
+			}
+			obj = { handler:this.onNoMatch };
+			arr = arr.concat(this.bwares[base] || []);
+		}
 		// Grab addl values from `info`
 		req.pathname = info.pathname;
 		req.search = info.search;
 		req.query = info.query;
 		// Exit if no middleware
-		let i=0, arr=this.wares, len=arr.length;
+		let i=0, len=arr.length;
 		if (len === i) return obj.handler(req, res);
 		// Otherwise loop thru all middlware
 		let next = err => err ? this.onError(err, req, res, next) : loop();
