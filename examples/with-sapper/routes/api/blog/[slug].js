5a1f6adb1055c9de9f896f201d1a96eafa0b9f98
Luke Edwards <luke.edwards05@gmail.com>
2018-02-03 10:19:11 -0800

    clone sapper-template

---
+++ examples/with-sapper/routes/api/blog/[slug].js
@@ -0,0 +1,22 @@
+import posts from './_posts.js';
+
+const lookup = new Map();
+posts.forEach(post => {
+	lookup.set(post.slug, JSON.stringify(post));
+});
+
+export function get(req, res, next) {
+	// the `slug` parameter is available because this file
+	// is called [slug].js
+	const { slug } = req.params;
+
+	if (lookup.has(slug)) {
+		res.set({
+			'Content-Type': 'application/json'
+		});
+
+		res.end(lookup.get(slug));
+	} else {
+		next();
+	}
+}
