1ace458b819ea5d37578a92abb49ad582d4c0193
Luke Edwards <luke.edwards05@gmail.com>
2018-02-28 07:27:10 -0800

    add `with-nuxtjs` example

---
+++ examples/with-nuxtjs/index.js
@@ -0,0 +1,29 @@
+const polka = require('polka');
+const { Nuxt, Builder } = require('nuxt');
+
+const { PORT=3000, NODE_ENV } = process.env;
+
+const dev = NODE_ENV !== 'production';
+const nuxt = new Nuxt({ dev });
+const app = polka();
+
+// Render every route with Nuxt.js
+app.use(nuxt.render);
+
+// Build only in dev mode with hot-reloading
+if (dev) {
+	new Builder(nuxt).build()
+	.then(listen)
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	})
+} else {
+	listen();
+}
+
+function listen() {
+	return app.listen(PORT).then(_ => {
+		console.log(`> Ready on localhost:${PORT}`);
+	});
+}
