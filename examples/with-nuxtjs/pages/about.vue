ff27865c1ad9d637aca9d5bf8cdce2d1d1febf34
Luke Edwards <luke.edwards05@gmail.com>
2018-02-28 07:29:23 -0800

    example: nuxt.js (#31)

---
+++ examples/with-nuxtjs/pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<h1>About Page</h1>
+		<nuxt-link to="/">Link to Home Page</nuxt-link>
+	</div>
+</template>
---
+++ examples/with-nuxtjs/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+	<div>
+		<h1>Home Page</h1>
+		<nuxt-link to="/about">Link to About Page</nuxt-link>
+	</div>
+</template>
