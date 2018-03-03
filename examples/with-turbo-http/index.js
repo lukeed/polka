3eea92e96268218e7958b7fc081d4359215730ac
Luke Edwards <luke.edwards05@gmail.com>
2018-03-03 13:57:30 -0800

    fix: send Buffer instead

--- examples/with-turbo-http/index.js
+++ examples/with-turbo-http/index.js
@@ -4,7 +4,7 @@ const http = require('turbo-http');
 const { PORT=3000 } = process.env;
 
 const { handler } = polka().get('/', (req, res) => {
-	res.end('Hello');
+	res.end(Buffer.from('Hello'));
 });
 
 http.createServer(handler).listen(PORT, err => {
