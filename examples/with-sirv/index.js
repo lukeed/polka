cdd7a58891e21bbc333f0352e7e8db3064dcce9e
Luke Edwards <luke.edwards05@gmail.com>
2018-05-14 11:22:57 -0700

    copy `with-serve-static` example w/ changes

---
+++ examples/with-sirv/index.js
@@ -0,0 +1,13 @@
+const polka = require('polka');
+
+const { PORT=3000 } = process.env;
+const serve = require('sirv')('public');
+
+polka()
+	.use(serve)
+	.get('/health', (req, res) => {
+		res.end('OK');
+	})
+	.listen(PORT).then(_ => {
+		console.log(`> Running on localhost:${PORT}`);
+	});
