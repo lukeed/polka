354127d2a02ae61e10ebbec9fc195bebb1d2f91e
Luke Edwards <luke.edwards05@gmail.com>
2019-03-02 06:58:06 -0800

    break(send): remove package;
    - too basic, pointless

--- packages/send/index.js
+++
@@ -1,6 +0,0 @@
-const { STATUS_CODES } = require('http');
-
-module.exports = function (res, code=200, data='', headers={}) {
-	res.writeHead(code, headers);
-	res.end(data || STATUS_CODES[code]);
-}
