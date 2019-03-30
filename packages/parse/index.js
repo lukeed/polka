49655d830b3b546e99918c75249240a909ff3156
Luke Edwards <luke.edwards05@gmail.com>
2019-03-30 16:55:51 -0700

    feat(parse): import changes for multiple types;
    - fix: use string for buffer concat (urlencoded)
    - generalize error messages
    - set encoding if/when defined

--- packages/parse/index.js
+++ packages/parse/index.js
@@ -1,6 +1,10 @@
-exports.json = function (opts={}) {
+const { parse } = require('querystring');
+
+const noop = x => x;
+
+exports.parse = function (opts={}) {
+	const { type, encoding='utf-8', parser=noop } = opts;
 	const limit = opts.limit || 100 * 1024; // 100kb
-	const type = opts.type || 'application/json';
 
 	return function (req, res, next) {
 		if (req._body) return next();
@@ -11,33 +15,59 @@ exports.json = function (opts={}) {
 		const clength = parseInt(head['content-length'], 10);
 
 		if (isNaN(clength) && head['transfer-encoding'] == null) return next(); // no body
-		if (ctype && !ctype.includes(type)) return next(); // not json
+		if (ctype && !ctype.includes(type)) return next(); // not valid type
 		if (clength === 0) return next(); // is empty
 
-		let bits = [];
+		if (encoding) {
+			req.setEncoding(encoding);
+		}
+
+		let bits = '';
 		let length = 0;
 		req.on('data', x => {
 			length += Buffer.byteLength(x);
 			if (length <= limit) {
-				bits.push(x);
+				bits += x;
 			} else {
-				next({
-					code: 413,
-					details: 'Exceeded JSON limit'
-				});
+				let err = new Error('Exceeded "Content-Length" limit');
+				err.code = 413;
 				req.destroy();
+				next(err);
 			}
 		}).on('end', () => {
 			try {
-				req.body = JSON.parse(bits);
+				req.body = parser(bits);
 				req._body = true;
 				next();
 			} catch (err) {
 				err.code = 422;
 				err.details = err.message;
-				err.message = 'Invalid JSON';
+				err.message = 'Invalid content';
 				next(err);
 			}
 		}).on('error', next);
 	};
+}
+
+exports.json = function (opts={}) {
+	const { limit, parser=JSON.parse } = opts;
+	const type = opts.type || 'application/json';
+	return exports.parse({ type, parser, limit });
+}
+
+exports.urlencoded = function (opts={}) {
+	const { parser=parse, limit } = opts;
+	const type = opts.type || 'application/x-www-form-urlencoded';
+	return exports.parse({ type, parser, limit });
+}
+
+exports.raw = function (opts={}) {
+	const { limit, encoding=null } = opts;
+	const type = opts.type || 'application/octet-stream';
+	return exports.parse({ limit, type, encoding });
+}
+
+exports.text = function (opts={}) {
+	const { limit, type='text/plain' } = opts;
+	return exports.parse({ limit, type });
 }
