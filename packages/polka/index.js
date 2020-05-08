b0a1af7bb99decb83d90d2680d3360bb29d27c41
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-05-07 21:41:18 -0400

    fix uncaught async error

--- packages/polka/index.js
+++ packages/polka/index.js
@@ -69,14 +69,10 @@ class Polka extends Router {
 		req.query = info.query || {};
 		req.search = info.search;
 
-		try {
-			let i=0, arr=obj.handlers.concat(this.onNoMatch), len=arr.length;
-			let loop = () => res.finished || (i < len) && arr[i++](req, res, next);
-			next = next || (err => err ? this.onError(err, req, res, next) : loop());
-			loop(); // init
-		} catch (err) {
-			this.onError(err, req, res, next);
-		}
+		let i=0, arr=obj.handlers.concat(this.onNoMatch), len=arr.length;
+		let loop = async () => res.finished || (i < len) && arr[i++](req, res, next);
+		next = next || (err => err ? this.onError(err, req, res, next) : loop().catch(next));
+		next(); // init
 	}
 }
 
