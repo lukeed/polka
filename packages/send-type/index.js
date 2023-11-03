4ae9c891536f5b014b44720c6327d35bcad91fa9
Aral Balkan <mail@ar.al>
2023-11-03 14:56:29 +0000

    Fixes #197: apply custom headers when passed a stream

--- packages/send-type/index.js
+++ packages/send-type/index.js
@@ -12,7 +12,8 @@ module.exports = function (res, code=200, data='', headers={}) {
 	let type = obj[TYPE] || res.getHeader(TYPE);
 
 	if (!!data && typeof data.pipe === 'function') {
-		res.setHeader(TYPE, type || OSTREAM);
+		obj[TYPE] = type || OSTREAM;
+		res.writeHead(code, obj);
 		return data.pipe(res);
 	}
 
